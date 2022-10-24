2c20c3f0c0bc89da4ff439bee665b727d146737c
7ThoughT7 <80511053+7ThoughT7@users.noreply.github.com>
2022-10-24 18:49:28 +0300

    Add files via upload

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -14,6 +14,10 @@ public class User implements UserDetails {
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
